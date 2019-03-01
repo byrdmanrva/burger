32597f555bf62597a5984e4ce06be7d9a46eae79
David Byrd <dbyrd@depparts.com>
2019-03-01 01:09:25 -0500

    homework

--- server.js
+++ server.js
@@ -1,25 +1,23 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override')
-var app = express();
+var express = require("express");
+
 var PORT = process.env.PORT || 8080;
 
-var db = process.env.DATABASE_URL || 'localhost'
+var app = express();
 
-app.use(express.static(process.cwd() + '/public'));
-app.use(bodyParser.urlencoded({
-	extended: false
-}))
-// override with POST having ?_method=DELETE
-app.use(methodOverride('_method'))
-var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
+app.use(express.static("public"));
 
-var routes = require('./controllers/burgers_controller.js');
-app.use('/', routes);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+var exphbs = require("express-handlebars");
 
-app.listen(PORT);
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
